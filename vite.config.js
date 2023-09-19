04e205d192086852a6c3d9f05c21b90e35b352aa
Aleš Kruba <aleskruba@seznam.cz>
2023-09-19 12:49:17 +0200

    dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/eshop-client/', // Change this to your repository name
   plugins: [react()],
 })
+
