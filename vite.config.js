f7613aa787445ce9a3683c99da357fe15fdf1997
Aleš Kruba <aleskruba@seznam.cz>
2023-09-20 12:17:10 +0200

    hope23333333356789015sdsdsdsss

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Change this to your repository name
+  base: '/', // Set it to the root path
   plugins: [react()],
 })
-
